0942bdeb58aaf065a5e6559a24ec112bb9ce0d32
moisesJurad0 <moises003@gmail.com>
2022-12-02 18:20:33 -0500

    refactor code

--- exam_api/searchById.py
+++ exam_api/searchById.py
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
     logger.info(f'event->{event}')
 
     table = dynamodb.Table(table_name)
-    question_id = int(event['pathParameters']['id'])
+    question_id = event['pathParameters']['id']
 
     response = table.query(KeyConditionExpression=Key('id').eq(question_id))
 
