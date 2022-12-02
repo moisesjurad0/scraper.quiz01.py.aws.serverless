fad2b266bc85a442ef6bc09b8f515f30d66cc13e
moisesJurado <mdjurado@indracompany.com>
2022-12-02 18:48:17 -0500

    cambio para forzar build

--- exam_api/app.py
+++ exam_api/app.py
@@ -1,12 +1,8 @@
 import logging
 import os
-
 import boto3
-
 
 logger = logging.getLogger('exam-api')
 logger.setLevel(logging.INFO)
-
 dynamodb = boto3.resource('dynamodb')
 table_name = os.environ.get('QUESTIONS_TABLE')
-
