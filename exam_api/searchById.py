0470d33f73cce3a6a447b0d8c546daf9c1343aa2
moisesJurad0 <moises003@gmail.com>
2022-12-02 16:49:11 -0500

    the app

--- exam_api/searchById.py
+++ exam_api/searchById.py
@@ -1,3 +1,4 @@
+import logging
 import simplejson as json
 import boto3
 import os
@@ -6,8 +7,14 @@ from boto3.dynamodb.conditions import Key
 dynamodb = boto3.resource('dynamodb')
 table_name = os.environ.get('QUESTIONS_TABLE')
 
+logger = logging.getLogger('exam-api')
+logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
+    
+    logger.info(f'event->{event}')
+    
+    
     table = dynamodb.Table(table_name)
     question_id = int(event['pathParameters']['id'])
     response = table.query(KeyConditionExpression=Key('id').eq(question_id))
