713c4acce0f9d0b65865f946923c365eac2c0e18
moisesJurad0 <moises003@gmail.com>
2022-12-03 13:24:33 -0500

    modificacion del search by text

--- exam_api/searchByText.py
+++ exam_api/searchByText.py
@@ -8,25 +8,37 @@ logger = app.logger
 dynamodb = app.dynamodb
 table_name = app.table_name
 
+
 def lambda_handler(event, context):
-    
+
     logger.info(f'event->{event}')
-    
-    
-    question = json.loads(event['body'])
+
+    request_json = json.loads(event['body'])
     table = dynamodb.Table(table_name)
-    
-    response=None
-    if question["id"] is not None:
-        response = table.query(KeyConditionExpression=Key('id').eq(question["id"]))
-        logger.info(f'response->{response}')
-    if question["text"] is not None:
-        response = table.scan(FilterExpression=Attr('question').contains(question["text"]) or Attr('answer').contains(question["text"]))
-        logger.info(f'response->{response}')
+
+    response = None
+
+    for i in ('id', 'UUID', 'question', 'answer', 'text'):
+        logger.info(f'i->{i}')
+        try:
+            if request_json[i] is not None:
+                if i in ('id', 'UUID'):
+                    response = table.query(
+                        KeyConditionExpression=Key(i).eq(request_json[i]))
+                elif i in ('question', 'answer'):
+                    response = table.scan(
+                        FilterExpression=Attr(i).contains(request_json[i]))
+                elif i == 'text':
+                    response = table.scan(
+                        FilterExpression=Attr('question').contains(request_json[i]) or
+                        Attr('answer').contains(request_json[i]))
+            logger.info(f'response->{response}')
+            break
+        except KeyError as e:
+            logger.warning(e, exc_info=True) 
 
     return{
         'statusCode': 200,
         'headers': {},
         'body': json.dumps(response['Items'])
     }
-
