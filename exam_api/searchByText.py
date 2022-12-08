2149793da71c162cf56bb72ff0c6a75a5d213aee
moisesJurado <mdjurado@indracompany.com>
2022-12-08 10:12:00 -0500

    update fields & format

--- exam_api/searchByText.py
+++ exam_api/searchByText.py
@@ -1,4 +1,9 @@
-import json
+"""_summary_.
+
+Returns:
+    _type_: _description_
+"""
+# import json
 
 import simplejson as json
 from boto3.dynamodb.conditions import Key, Attr
@@ -10,7 +15,15 @@ table_name = app.table_name
 
 
 def lambda_handler(event, context):
+    """_summary_.
 
+    Args:
+        event (_type_): _description_
+        context (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     logger.info(f'event->{event}')
 
     request_json = json.loads(event['body'])
@@ -18,26 +31,36 @@ def lambda_handler(event, context):
 
     response = None
 
-    for i in ('id', 'UUID', 'question', 'answer', 'text'):
+    for i in (
+        'id',
+        'uuid',
+        'question',
+        'question_type',
+        'answer',
+        'text',
+            'correct'):
         logger.info(f'i->{i}')
         try:
             if request_json[i] is not None:
                 if i in ('id'):
                     response = table.query(
                         KeyConditionExpression=Key(i).eq(request_json[i]))
-                elif i in ('UUID', 'question', 'answer'):
+                elif i in ('uuid', 'question', 'question_type', 'answer'):
                     response = table.scan(
                         FilterExpression=Attr(i).contains(request_json[i]))
                 elif i == 'text':
+                    response = table.scan(FilterExpression=(
+                        Attr('question').contains(request_json[i]) or
+                        Attr('answer').contains(request_json[i])))
+                elif i == 'correct':
                     response = table.scan(
-                        FilterExpression=Attr('question').contains(request_json[i]) or
-                        Attr('answer').contains(request_json[i]))
+                        FilterExpression=Attr(i).eq(request_json[i]))
                 logger.info(f'response->{response}')
                 break
         except KeyError as e:
             logger.warning(e, exc_info=True)
 
-    return{
+    return {
         'statusCode': 200,
         'headers': {},
         'body': json.dumps(response['Items'])
