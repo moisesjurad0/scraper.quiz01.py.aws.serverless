a3eb38737b4ba9075ccc1f6b343c7f880efcdca8
moisesJurad0 <moises003@gmail.com>
2022-12-08 10:12:00 -0500

    update fields & format

--- exam_api/create.py
+++ exam_api/create.py
@@ -1,3 +1,8 @@
+"""_summary_.
+
+Returns:
+    _type_: _description_
+"""
 import json
 import uuid
 import app
@@ -6,19 +11,28 @@ logger = app.logger
 dynamodb = app.dynamodb
 table_name = app.table_name
 
+
 def lambda_handler(event, context):
-    
+    """_summary_.
+
+    Args:
+        event (_type_): _description_
+        context (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     logger.info(f'event->{event}')
-    
-    question = json.loads(event['body'])        
+
+    question = json.loads(event['body'])
     question['id'] = uuid.uuid4().hex
-    
+
     table = dynamodb.Table(table_name)
     response = table.put_item(TableName=table_name, Item=question)
-    
-    logger.info(f'response->{response}')    
-    
-    return{
+
+    logger.info(f'response->{response}')
+
+    return {
         'statusCode': 201,
         'headers': {},
         'body': json.dumps({
