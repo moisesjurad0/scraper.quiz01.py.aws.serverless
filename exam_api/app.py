8fc0f44e2a31fb3af0d786a73e0c6e1866030ba9
moisesJurad0 <moises003@gmail.com>
2022-12-12 20:30:51 -0500

    cambios para la segunda api para el examen02

--- exam_api/app.py
+++ exam_api/app.py
@@ -2,7 +2,7 @@ import logging
 import os
 import boto3
 
-logger = logging.getLogger('exam-api')
+logger = logging.getLogger('exam02-api')
 logger.setLevel(logging.INFO)
 dynamodb = boto3.resource('dynamodb')
-table_name = os.environ.get('QUESTIONS_TABLE')
+table_name = os.environ.get('API_TABLE')
