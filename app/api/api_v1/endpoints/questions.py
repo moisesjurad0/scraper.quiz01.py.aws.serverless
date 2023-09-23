e3b802ad66f308acc0bbbcd7294761ec39ef3e46
Moisés Jurado <moises003@gmail.com>
2023-09-23 18:36:49 -0500

    update

--- app/api/api_v1/endpoints/questions.py
+++ app/api/api_v1/endpoints/questions.py
@@ -7,7 +7,7 @@ from typing import Union
 import json
 # import simplejson as sjson
 from typing import Union
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, PositiveInt
 from boto3.dynamodb.conditions import Key, Attr
 # from boto3.dynamodb.conditions import Key, logical_and
 from enum import Enum
@@ -21,26 +21,52 @@ dynamodb = boto3.resource('dynamodb')
 table_name = os.environ.get('API_TABLE_PYSICAL_ID')
 
 
-class Answer(BaseModel):
-    answer_text: str = 'Celeste'
-    is_correct: Union[bool, None] = False
-
-
 class QuestionType(str, Enum):
     CHECK = 'CHECK'
     RADIO = 'RADIO'
     RADIO_BOOL = 'RADIO_BOOL'
 
+# class Question(BaseModel):
+#     id: str = '¿De que colores es un Arco Iris?'
+#     exam_number: int = 1 #''#
+#     question_type: str = 'CHECK'  # CHANGE THIS TO QuestionType, set default to CHECK
+#     answer_text: str = 'Celeste' #''#
+#     is_correct: bool # Union[bool, None] = None
+#     last_modified: str = '2023-09-214T13:41:38.446131' #''#
+
 
 class Question(BaseModel):
-    # id: str
-    id: str = '¿De que colores es un Arco Iris?'
-    # question_text: str = '¿De que colores es un Arco Iris?'
-    question_type: str = 'CHECK'  # CHANGE THIS TO QuestionType, set default to CHECK
-    last_modified: str = '2023-09-214T13:41:38.446131'
-    # answers: Optional[List[Answer]]
-    # answers: Optional[List[Answer]] = List[Answer(), Answer()]
-    answers: Optional[List[Answer]] = Field(default=[Answer(), Answer()])
+    id: str
+    exam_number: int  # PositiveInt
+    question_type: str
+    answer_text: str
+    is_correct: bool
+    last_modified: str
+
+
+# class QuestionQuery(BaseModel):
+#     id: Optional[str] = None
+#     exam_number: Optional[int] = None  # Optional[PositiveInt]
+#     question_type: Optional[str] = None
+#     answer_text: Optional[str] = None
+#     is_correct: Union[bool, None] = None
+#     last_modified: Optional[str] = None
+
+# class QuestionQuery(BaseModel):
+#     id: str = None
+#     exam_number: Optional[int] = None  # Optional[PositiveInt]
+#     question_type: str = None
+#     answer_text: str = None
+#     is_correct: Union[bool, None] = None
+#     last_modified: str = None
+
+class QuestionQuery(BaseModel):
+    id: Optional[str] = None
+    exam_number: Optional[int] = None  # Optional[PositiveInt]
+    question_type: Optional[str] = None
+    answer_text: Optional[str] = None
+    is_correct: Optional[Union[bool, None]] = None
+    last_modified: Optional[str] = None
 
 
 @router.get("/")
@@ -48,95 +74,87 @@ async def root():
     return {"message": "Get Questions!"}
 
 
-@router.get("/{id}")
-def read_item(id: str):
-    table = dynamodb.Table(table_name)
-    response = table.query(KeyConditionExpression=Key('id').eq(id))
-    logger.info(f'response->{response}')
-
-    return {
-        'statusCode': 200,
-        'headers': {},
-        'body': json.dumps(response['Items'])
-    }
-
-
-@router.delete("/{id}")
-def delete_item(id: str):
+@router.get("/{id}/{answer_text}")
+def read_item(id: str, answer_text: str):
     table = dynamodb.Table(table_name)
     # response = table.query(KeyConditionExpression=Key('id').eq(id))
-    response = table.delete_item(Key={'id': id})
+    # response = table.query(Key={'id': id,'answer_text': answer_text})
 
-    logger.info(f'response->{response}')
+    response = table.query(
+        KeyConditionExpression=(
+            Key('id').eq(id) &
+            Key('answer_text').eq(answer_text)
+        )
+    )
 
-    return response
+    logger.info(f'response=>{response}')
     # return {
     #     'statusCode': 200,
     #     'headers': {},
     #     'body': json.dumps(response['Items'])
     # }
+    return response
 
 
-@router.post("/")
-def create_item(item: Question):
+@router.delete("/{id}/{answer_text}")
+def delete_item(id: str, answer_text: str):
     table = dynamodb.Table(table_name)
-    response = table.put_item(TableName=table_name, Item=item.dict())
-
-    logger.info(f'response->{response}')
-
-    return {
-        'statusCode': 201,
-        'headers': {},
-        'body': json.dumps({
-            'message': 'Item Created',
-            'id': item.id})
-    }
+    # response = table.query(KeyConditionExpression=Key('id').eq(id))
+    response = table.delete_item(Key={'id': id, 'answer_text': answer_text})
+    logger.info(f'response=>{response}')
+    # return {
+    #     'statusCode': 200,
+    #     'headers': {},
+    #     'body': json.dumps(response['Items'])
+    # }
+    return response
 
 
-@router.post("/search/contains")
-def search_items(question: Question):
+@router.put("/")
+def put_item(item: Question):
+    table = dynamodb.Table(table_name)
+    response = table.put_item(Item=item.dict())
+    logger.info(f'response=>{response}')
+    # return {
+    #     'statusCode': 201,
+    #     'headers': {},
+    #     'body': json.dumps({
+    #         'message': 'Item Created',
+    #         'id': item.id})
+    # }
+    return response
+
+
+@router.post("/contains")
+def search_items(question: QuestionQuery):
 
     table = dynamodb.Table(table_name)
     filter_conditions = []
 
     if question:
+
         for i_prop_name, i_prop_value in question.dict().items():
+
             if i_prop_value is not None:
-                
+
                 logger.info(f'i:{i_prop_name}=>{i_prop_value}')
-                
-                if i_prop_name == 'id':
-                    
-                    filter_conditions.append(Key(i_prop_name).begins_with(i_prop_value))
-                    
-                elif i_prop_name == 'answers':
-                                        
-                    for j_answer in i_prop_value: # iterate all answers 1 by 1
-                        
-                        logger.info(f'j:{j_answer}')
-                        for k_prop_name, k_prop_value in j_answer.items(): # iterate all props in 1 answer
-                            
-                            if k_prop_value is not None:
-                                
-                                logger.info(f'k:{k_prop_name}=>{k_prop_value}')
 
-                                # if k_prop_name == 'is_correct':
-                                #     filter_conditions.append(Attr(f'{i_prop_name}.{k_prop_name}').eq(k_prop_value))
-                                # else :
-                                #     filter_conditions.append(Attr(f'{i_prop_name}.{k_prop_name}').begins_with(k_prop_value))
-                                    
-                elif i_prop_name == 'question_type':
-
-                    filter_conditions.append(Attr(i_prop_name).eq(i_prop_value))
-                else:
-
-                    filter_conditions.append(Attr(i_prop_name).begins_with(i_prop_value))
+                if i_prop_name in {'id', 'answer_text'}:  # KEY begins_with
+                    filter_conditions.append(
+                        Key(i_prop_name).begins_with(i_prop_value))
+                elif i_prop_name in {'question_type', 'is_correct', 'exam_number'}:  # ATTR eq
+                    filter_conditions.append(
+                        Attr(i_prop_name).eq(i_prop_value))
+                else:  # ATTR else
+                    filter_conditions.append(
+                        Attr(i_prop_name).begins_with(i_prop_value))
 
     if filter_conditions:
-        response = table.scan(FilterExpression=reduce(lambda x,y: x & y,filter_conditions))
+        response = table.scan(
+            FilterExpression=reduce(lambda x, y: x & y, filter_conditions))
     else:
         # No filters provided, retrieve all items
         response = table.scan()
-        
+
     logger.info(f'm01.response=>{response}')
     return response
